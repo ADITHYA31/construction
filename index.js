571bf5662531d631b02b0e851d98b12068c4aac4
ADITHYA31 <adithyavatti@gmail.com>
2023-10-12 19:42:54 +0530

    port change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
